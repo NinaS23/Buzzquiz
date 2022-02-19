63ecb903d90e9ee2c744d3b53bf2047b720856a3
Joyceoliveiragg <joyce.ggomes@ufpe.br>
2022-02-19 16:06:52 -0300

    finalizacao do quiz

--- scripito.js
+++ scripito.js
@@ -1,19 +1,31 @@
 var acertos = 0
+var respostas = 0
 var quizz
+var level
 takeThisQuizz()
-function takeThisQuizz(quizz) {
+function takeThisQuizz() {
 
     //const oQuizz = quizz.id  ${id}
     var promQuizz = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/2741")
     promQuizz.then(renderOQuizz);
     /*document.querySelector("")*/
 }
+
+
+function comparador(){
+    return Math.random() - 0.5;
+  }
+
+  
 function renderOQuizz(raaa) {
-    quizz = raaa.data;
+    acertos = 0
+    respostas = 0
+    if(!quizz)
+        quizz = raaa.data;
     let quizz_title = quizz.title;
     let quizz_image = quizz.image
     let quizz_questions = quizz.questions;
-    let level = quizz.levels
+    level = quizz.levels
 
     let banner = document.querySelector(".banner");
     banner.innerHTML = `<img src="${quizz_image}"/>
@@ -23,28 +35,30 @@ function renderOQuizz(raaa) {
     //  banner();
     // 1. onclick-> pega a id do quizz, tbm manda o axios pegar quizz, renderizar o q vier da promessa
 
-
+    let pergunta = document.querySelector(".perguntas");
+    pergunta.innerHTML = ""
     for (let i = 0; i < quizz_questions.length; i++) {
         let question = quizz_questions[i];
         let question_title = question.title
         console.log(question_title)
 
 
-        let pergunta = document.querySelector(".perguntas");
-        pergunta.innerHTML += `<div class="pergunta" data-identifier="question">
+        
+
+        pergunta.innerHTML += `<div id="pergunta-${i}" class="pergunta" data-identifier="question">
             <div class="titulo">${question_title}</div>
             <div class="respostas p${i}"></div>
             </div>
             `
-            
-
+        
+            question.answers.sort(comparador)
         for (let j = 0; j < question.answers.length; j++) {
             let respostas = question.answers[j]
             let image = respostas.image
             let text = respostas.text
             let resp = document.querySelector(`.p${i}`)
             resp.innerHTML += `
-                <div id = "${i}-${j}" class="resposta" onclick = "respostaSelecionada(${j},${i})"  onclick = "selecionada(${j},${i})" data-identifier="answer">
+                <div id = "${i}-${j}" class="resposta" onclick = "respostaSelecionada(${j},${i})"  data-identifier="answer">
                     <img  src="${image}" >
                     <div class="texto" id = "${i}-${j}">${text}</div>
                 </div>
@@ -62,7 +76,7 @@ function renderOQuizz(raaa) {
             // exibir titulo e imagem referente ao level 
             //Math.ceil, Math.floor, Math.round   
         }
-
+        
 
     }   // botao  reiniciar quizz -> deve voltar para o topo da tela e as respostas devem estar zeradas 
     // botao voltar para home ->Tela 1: Lista de Quizzes
@@ -71,34 +85,14 @@ function renderOQuizz(raaa) {
 
 
 
-    for (let i = 0; i < level.length; i++) {
-        let nivel = level[i];
-        let titulo = nivel.title
-        let imgN = nivel.image
-        let texto = nivel.text
-        let valor = nivel.minValue
-        let nivelss = document.querySelector(`.final`);
-        nivelss.innerHTML = `
-        <div class="resultado-quizz" ${nivel} data-identifier="quizz-result">
-            <div class="titulo">${valor}: ${titulo}</div>
-            <div class="conteudo">
-                <img src="${imgN}">
-                <div class="texto">${texto}</div>
-            </div>
-            <button class="reiniciar"  onclick="resetQuizz()">Reiniciar Quizz</button>  
-            <button class="voltar"  onclick="loadMainPage()">Voltar para home</button>  
-        </div>`
-
-
-    }
-
-
-
 }
 
 
 function respostaSelecionada(resposta, pergunta) {
     
+    let textOClica = document.getElementById(`${pergunta}-${resposta}`)
+    
+    respostas ++;
     if (quizz.questions[pergunta].respondida){
         
         return
@@ -108,10 +102,13 @@ function respostaSelecionada(resposta, pergunta) {
         acertos ++
 
     }
-    
-            
+
+
+   
     for (let i = 0; i < quizz.questions[pergunta].answers.length; i++) {
         let textO = document.getElementById(`${pergunta}-${i}`)
+        if(textO != textOClica) 
+            textO.classList.add("selecionada") 
         if (quizz.questions[pergunta].answers[i].isCorrectAnswer) {
            
             textO.classList.add("alternativa-certa")
@@ -121,8 +118,48 @@ function respostaSelecionada(resposta, pergunta) {
         }
         
     }
-    
+    let proxPergunta = document.getElementById(`pergunta-${pergunta+1}`)
+    if(respostas == quizz.questions.length){
+        calcularPontuacao()
+        proxPergunta = document.getElementById("resultado")
+    }
+    setTimeout(()=>{
+        proxPergunta.scrollIntoView({behavior: 'smooth'})
+    },1000)
+
+  
 }
+
+function arredondarPontuacao(){
+    let nivel = (acertos/quizz.questions.length) *100
+    let arredondamento = Math.round(nivel)
+    return arredondamento
+}
+
+
+function calcularPontuacao (){
+    //descobrir  qual nivel eu estou.
+    let nivel = level[0];
+    let pontuacao =  arredondarPontuacao()
+    for (let i = 0; i < level.length; i++) {
+        let aux = level[i];
+        if(pontuacao > aux.minValue){
+            nivel = aux
+        }
+    }
+    let nivelss = document.querySelector(`.final`);
+    nivelss.innerHTML = `
+           <div id="resultado" class="resultado-quizz"data-identifier="quizz-result">
+               <div class="titulo">${pontuacao}% ${nivel.title}</div>
+               <div class="conteudo">
+                   <img src="${nivel.image}">
+                   <div class="texto">${nivel.text}</div>
+               </div>
+               <button class="reiniciar"  onclick="resetQuizz()">Reiniciar Quizz</button>  
+               <button class="voltar"  onclick="loadMainPage()">Voltar para home</button>  
+           </div>`
+}
+
 function selecionada(){
 for (let i = 0; i < quizz.questions.length; i++) {
     let textO = document.getElementById(`${i}-${j}`)
@@ -153,10 +190,17 @@ function arredonda(x) {
     
 function resetQuizz(){
   //fazer o scroll ir pra cima
-  let caixaResultadoEsconder = document.querySelector(".resultado-quizz")
-  caixaResultadoEsconder.classList.add("esconder")
+  let caixaResultadoEsconder = document.getElementById("resultado")
+  caixaResultadoEsconder.classList.toggle("esconder")
   //zerar respostas(aqui preciso de joy)
+  //calcularPontuacao()
 
+  let banner = document.getElementById("banner")
+
+setTimeout(()=>{
+   banner.scrollIntoView({behavior: 'smooth'})
+   renderOQuizz()
+},1000)
 }
 
 function loadMainPage(){
@@ -170,3 +214,24 @@ SumirFinalDiv.classList.add("esconder")
 tituloClasseSUMIR.classList.add("esconder")
 console.log(loadMainPage())
 }
+
+/*
+Maria Bevenuto11:16
+function ValidarQuizz() {
+    let urlQuizz = document.querySelector(".Url_quizz")
+    let tituloquizz = document.querySelector(".titulo_quizz").value
+    let url = document.getElementsByName('url')
+    let h6 = document.querySelector("h6")
+        var expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+        var regex = new RegExp(expression);
+        var t = 'www.google.com';
+
+        if (t.match(regex) && urlQuizz.value  !== "") {
+           
+Maria Bevenuto11:26
+math.round(nivel)
+Maria Bevenuto12:13
+function arredonda(x) {
+let inteiroProximo = Math.round(x);
+return inteiroProximo;
+}*/
